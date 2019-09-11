cbee234f10d8de9cb8847a99864a487a3b8bab92
dabutvin <butvinik@outlook.com>
2019-09-11 10:08:40 -0700

    put in a bunch of real data

---
+++ src/tree/index.js
@@ -0,0 +1,91 @@
+const data = {
+  name: "Issac and Celia Sklar",
+  children: [
+    {
+      name: "Nathan",
+      children: [
+        { name: "Harriet", children: [{ name: "Susan" }, { name: "Loraine" }] },
+        { name: "Charles", children: [{ name: "David" }, { name: "???" }] },
+        { name: "Marion", children: [{ name: "Helene" }, { name: "Cynthia" }] }
+      ]
+    },
+    {
+      name: "William",
+      years: "???  - 1958",
+      children: [
+        { name: "Bobby", children: [{ name: "Mike" }, { name: "Amy" }] },
+        { name: "Florence", children: [{ name: "Jae" }, { name: "Laurie" }] },
+        { name: "Myra", children: [{ name: "Wendy" }, { name: "Jeffrey" }] }
+      ]
+    },
+    { name: "Sophie", children: [{ name: "Morty" }, { name: "Milton" }] },
+    {
+      name: "Mary",
+      children: [{ name: "Gertrude" }, { name: "Lillian" }, { name: "Arnold" }]
+    },
+    {
+      name: "Beatrice",
+      years: "1902 - 1978",
+      children: [
+        {
+          name: "Charlotte",
+          years: "1926 - 2019",
+          children: [
+            {
+              name: "Scott",
+              years: "1949 - ",
+              children: [{ name: "Adam" }, { name: "Josh" }]
+            },
+            {
+              name: "Arthur",
+              years: "1951 - ",
+              children: [{ name: "Debbie" }, { name: "Lisa" }]
+            },
+            {
+              name: "Carol",
+              years: "1954 - ",
+              children: [{ name: "Jonathan" }, { name: "Michelle" }]
+            }
+          ]
+        },
+        {
+          name: "Marilyn",
+          years: "1931 - ",
+          children: [
+            { name: "Cheryl", years: "1954 - " },
+            {
+              name: "Bonnie",
+              years: "1958 - ",
+              children: [
+                { name: "James" },
+                { name: "Elizabeth" },
+                { name: "Kimberly", children: [{ name: "Lily" }] }
+              ]
+            },
+            {
+              name: "Warren",
+              years: "1960 - ",
+              children: [
+                { name: "Karen", years: "1986 - " },
+                {
+                  name: "Jennifer",
+                  years: "1988 - ",
+                  children: [
+                    { name: "Sophia" },
+                    { name: "Derrick" },
+                    { name: "Leah" }
+                  ]
+                },
+                { name: "Daniel", years: "1989 - " }
+              ]
+            }
+          ]
+        },
+        { name: "Marty", years: "1940 - 2017" }
+      ]
+    },
+    { name: "Daniel", children: [{ name: "Allen" }] },
+    { name: "Max", children: [{ name: "Jay" }, { name: "Barry" }] }
+  ]
+};
+export { data };
--- src/views/Tree.vue
+++ src/views/Tree.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <organization-chart :datasource="ds">
+    <organization-chart :datasource="ds" :pan="true" :zoom="true">
       <template slot-scope="{ nodeData }">
         <div class="title">{{ nodeData.name }}</div>
         <div v-if="nodeData.years" class="content">
@@ -18,7 +18,7 @@
 import Vue from "vue";
 import OrganizationChart from "vue-organization-chart";
 import "vue-organization-chart/dist/orgchart.css";
-
+import { data } from "../tree";
 import defaultImg from "../assets/person.svg";
 
 export default {
@@ -28,32 +28,7 @@ export default {
   data() {
     return {
       defaultImg: defaultImg,
-      ds: {
-        id: "1",
-        name: "Family Tree",
-        children: [
-          { id: "2", name: "Bo Miao", years: "department manager" },
-          {
-            id: "3",
-            name: "Su Miao",
-            years: "department manager",
-            children: [
-              { id: "4", name: "Tie Hua", years: "senior engineer" },
-              {
-                id: "5",
-                name: "Hei Hei",
-                years: "senior engineer",
-                children: [
-                  { id: "6", name: "Pang Pang", years: "engineer" },
-                  { id: "7", name: "Xiang Xiang", years: "UE engineer" }
-                ]
-              }
-            ]
-          },
-          { id: "8", name: "Hong Miao", years: "department manager" },
-          { id: "9", name: "Chun Miao", years: "department manager" }
-        ]
-      }
+      ds: data
     };
   }
 };
